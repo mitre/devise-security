f16506bfb8035ff6434f7a3bdf5a1731d9de5e6f
Aaron Lippold <lippold@gmail.com>
2026-02-21 13:47:08 -0500

    docs: YARD documentation for all model modules
    Add @param, @return, @see, @deprecated tags to:
    - compatibility.rb
    - database_authenticatable_patch.rb
    - security_questionable.rb
    
    Authored by: Aaron Lippold<lippold@gmail.com>

--- lib/devise-security/models/security_questionable.rb
+++ lib/devise-security/models/security_questionable.rb
@@ -2,19 +2,27 @@
 
 module Devise
   module Models
-    # SecurityQuestionable is an accessible add-on for visually handicapped people,
-    # to ship around the captcha with screenreader compatibility.
+    # SecurityQuestionable provides an accessible alternative to CAPTCHAs
+    # for screenreader-compatible authentication flows. Users select a
+    # security question at registration and answer it on sensitive forms
+    # (unlock, password reset, confirmation).
     #
-    # You need to add two text_field_tags to the associated forms (unlock,
-    # password, confirmation):
-    # :security_question_answer and :captcha
+    # == Form Setup
+    # Add to unlock, password, and confirmation forms:
+    #   text_field_tag :security_question_answer
+    #   text_field_tag :captcha
     #
-    # And add the security_question to the register/edit form.
-    # f.select :security_question_id, SecurityQuestion.where(locale: I18n.locale).map{|s| [s.name, s.id]}
-    # f.text_field :security_question_answer
+    # Add to registration/edit forms:
+    #   f.select :security_question_id, SecurityQuestion.where(locale: I18n.locale).map { |s| [s.name, s.id] }
+    #   f.text_field :security_question_answer
+    #
+    # @see SecurityQuestion the model storing available questions per locale
+    # @see Devise::Models::DatabaseAuthenticatable required base module
     module SecurityQuestionable
       extend ActiveSupport::Concern
 
+      # @param _klass [Class] the model class including this module
+      # @return [Array<Symbol>] required database columns
       def self.required_fields(_klass)
         [:security_question_id, :security_question_answer]
       end
